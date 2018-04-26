4bd0c808b1283cd799ccb8656f58f01b03f90e96
tdoan5 <33384826+tdoan5@users.noreply.github.com>
2018-04-26 16:52:07 -0400

    Update deploy_kubernetes.sh

--- deploy_kubernetes.sh
+++ deploy_kubernetes.sh
@@ -2,8 +2,6 @@ gcloud services enable container.googleapis.com
 
 gcloud container clusters create todoapp-cluster
 
-kubectl run cc5550 --image=tdoan5/cs5550-rest:v2 --port=5000
+kubectl run cc5550 --image=tdoan5/cs5550-demo --port=5000
 
 kubectl expose deployment cc5550 --type=LoadBalancer --port 80 --target-port 5000
-
-kubectl get service cc5550
