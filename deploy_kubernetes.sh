9c051640eab4a42908d3c03d4ab9663830d72241
tdoan5 <33384826+tdoan5@users.noreply.github.com>
2018-05-10 23:22:49 -0400

    Delete deploy_kubernetes.sh

--- deploy_kubernetes.sh
+++
@@ -1,7 +0,0 @@
-gcloud services enable container.googleapis.com
-
-gcloud container clusters create todoapp-cluster
-
-kubectl run cc5550 --image=tdoan5/cs5550-demo --port=5000
-
-kubectl expose deployment cc5550 --type=LoadBalancer --port 80 --target-port 5000
