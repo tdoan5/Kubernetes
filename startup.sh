faa004184c92b2e8eae9cd45c34d6baf238686a9
tdoan5 <33384826+tdoan5@users.noreply.github.com>
2018-05-11 01:15:36 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,7 +8,7 @@ pip3 install --upgrade pymongo
 # add -N so that wget do not duplicate copied files when reset 
 
 sudo wget -N --directory-prefix=$HOME/ \
-https://www.dropbox.com/s/0u2292pl7bwkg22/todolist_api.py
+https://raw.githubusercontent.com/tdoan5/Kubernetes/tdoan5-proj/todolist_api.py
 
 # launch the app
 python3 todolist_api.py
