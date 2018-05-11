4a9be4589d96f7bd05dc1f9bf8306c97c6c15e3b
tdoan5 <33384826+tdoan5@users.noreply.github.com>
2018-05-10 23:23:04 -0400

    Delete deploy_api.sh

--- deploy_api.sh
+++
@@ -1,3 +0,0 @@
-gcloud compute firewall-rules create rule-allow-tcp-6000 --source-ranges 0.0.0.0/0 --target-tags http-server --allow tcp:6000
-
-gcloud compute instances create test-inst --machine-type n1-standard-1 --image-family debian-8 --image-project debian-cloud --tags http-server --metadata-from-file startup-script=./startup.sh
