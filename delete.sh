6e5e754a8bae14d0623f370a931593c827416188
tdoan5 <33384826+tdoan5@users.noreply.github.com>
2018-05-11 22:48:37 -0400

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,5 +1,11 @@
-gcloud compute instances delete cisc5550-api
+#gcloud compute instances delete cisc5550-api
 
 kubectl delete service cc5550
+
+kubectl delete deploy cc5550
+
+kubectl delete service cc5550api
+
+kubectl delete deploy cc5550api
 
 gcloud container clusters delete cisc5550-cluster
